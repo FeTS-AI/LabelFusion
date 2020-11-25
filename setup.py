f208d92fd6b44c7e8a5cb214697eeaba60bd6813
sarthakpati <sarthak.pati@hotmail.com>
2020-11-25 16:07:43 -0500

    added setup

---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+  readme = readme_file.read()
+
+requirements = [
+  'numpy',
+  'SimpleITK==1.2.4',
+  'setuptools',
+]
+
+setup(
+  name='LabelFusion',
+  version='0.0.1.NR', # NR: non-release; this should be changed when tagging\
+  author="Megh Bhalerao, Sarthak Pati",
+  author_email='software@cbica.upenn.edu',
+  python_requires='>=3.6',
+  scripts=['fusion_run'],
+  classifiers=[
+    'Development Status :: Pre-Alpha',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: BSD-3-Clause License',
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+  description=(
+    "Label fusion strategies for multi-class labels."
+  ),
+  install_requires=requirements,
+  license="BSD-3-Clause License",
+  long_description=readme,
+  long_description_content_type='text/markdown',
+  include_package_data=True,
+  keywords='semantic, segmentation, brain, breast, liver, lung, label-fusion, fusion',
+  zip_safe=False,
+)
