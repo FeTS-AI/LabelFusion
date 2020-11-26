ef0f0b9c0a6d0bc6722f4f988f24a1121ae358c0
sarthakpati <sarthak.pati@hotmail.com>
2020-11-26 09:06:41 -0500

    removed redundant code

--- Methods/wrapper.py
+++ Methods/wrapper.py
@@ -10,5 +10,3 @@ def fuse_segmentations(list_of_oneHotEncodedSegmentations, method, class_list):
     return majority_voting(list_of_oneHotEncodedSegmentations)
   elif 'simple' in method:
     return simple_iterative(list_of_oneHotEncodedSegmentations)
-
-  test = 1
