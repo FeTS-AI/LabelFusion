abf829a7f2394b90d132effed2480129309ecc7f
sarthakpati <sarthak.pati@hotmail.com>
2020-11-26 09:07:52 -0500

    added refs

--- Methods/wrapper_itk.py
+++ Methods/wrapper_itk.py
@@ -5,6 +5,6 @@ def fuse_segmentations_itk(list_of_segmentations_images, method):
   This function takes a list of segmentations images and the method as input and returns the fused segmentation for ITK implementations
   '''
   if 'staple' in method:
-    return sitk.MultiLabelSTAPLE(list_of_segmentations_images)
+    return sitk.MultiLabelSTAPLE(list_of_segmentations_images) # DOI: 10.1109/TMI.2004.830803
   elif 'voting' in method:
-    return sitk.LabelVotingImageFilter(list_of_segmentations_images)
+    return sitk.LabelVotingImageFilter(list_of_segmentations_images) # DOI: 10.1016/j.patrec.2005.03.017
