8e5753e5cc2617f3c8b9f874662b8292defbd88e
sarthakpati <sarthak.pati@hotmail.com>
2020-11-26 09:01:45 -0500

    updated comment

--- Methods/wrapper.py
+++ Methods/wrapper.py
@@ -4,13 +4,11 @@ from .staple import *
 
 def fuse_segmentations(list_of_oneHotEncodedSegmentations, method, class_list):
   '''
-  This function takes a list of one-hot encoded segmentations and the method as input and returns the one-hot encoded fused segmentation
+  This function takes a list of one-hot encoded segmentations and the method as input and returns the one-hot encoded fused segmentation for non-ITK implementations
   '''
   if 'majority' in method:
     return majority_voting(list_of_oneHotEncodedSegmentations)
   elif 'simple' in method:
     return simple_iterative(list_of_oneHotEncodedSegmentations)
-  elif 'staple' in method:
-    return staple(list_of_oneHotEncodedSegmentations, class_list)
 
   test = 1
