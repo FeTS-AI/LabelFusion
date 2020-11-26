b59d6e81cb0702c2e3ab68ec65515c3fcb321b84
sarthakpati <sarthak.pati@hotmail.com>
2020-11-26 08:43:14 -0500

    finished implementation

--- Methods/simple.py
+++ Methods/simple.py
@@ -9,17 +9,16 @@ def simple_iterative(segmentationArray_oneHot):
   Reference: DOI:10.1109/tmi.2010.2057442
   '''
   segmentationArray_oneHot_wrap = segmentationArray_oneHot
-  init_seg = majority_voting(segmentationArray_oneHot_wrap) # use majority voting as initial
+  seg_for_comparision = majority_voting(segmentationArray_oneHot_wrap) # use majority voting as initial
   
   dice_list = []
   num_classes = segmentationArray_oneHot_wrap[0].shape[0]
   # calculate dice for each input compared to initial segmentation
   for i in range(0, len(segmentationArray_oneHot_wrap)): 
-    dice_list.append(1 - MCD_loss(segmentationArray_oneHot_wrap[i], init_seg, num_classes))
+    dice_list.append(1 - MCD_loss(segmentationArray_oneHot_wrap[i], seg_for_comparision, num_classes))
 
   num_iter = len(segmentationArray_oneHot_wrap) - 1 # initialize the number of iterations
 
-  seg_sum = segmentationArray_oneHot[0]
   for i in range(num_iter):
     order = np.array(dice_list).argsort() # sort the dice
     # remove the first segmentation because 
@@ -27,10 +26,12 @@ def simple_iterative(segmentationArray_oneHot):
     del segmentationArray_oneHot_wrap[order[0]]
 
     dice_list = [] # re-initilize dice list to get iteratively updated 
+    seg_sum = np.zeros(segmentationArray_oneHot[0].shape) # initialize the first segmentation
     for i in range(0, len(segmentationArray_oneHot_wrap)): 
-      current_dice = 1 - MCD_loss(segmentationArray_oneHot_wrap[i], init_seg, num_classes)
+      current_dice = 1 - MCD_loss(segmentationArray_oneHot_wrap[i], seg_for_comparision, num_classes)
       dice_list.append(current_dice)
       seg_sum += current_dice * segmentationArray_oneHot_wrap[i]
-    
 
-  return init_seg
+    seg_for_comparision = (seg_sum/(sum(dice_list))>0.5).astype(int) # update the seg_for_comparision    
+
+  return seg_for_comparision
