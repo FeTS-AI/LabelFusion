23cb7cfa6591e9a8829d9e119401ddea496fb9aa
sarthakpati <sarthak.pati@hotmail.com>
2020-12-16 05:13:52 -0500

    consolidating code

--- Methods/fusion_wrappers.py
+++ Methods/fusion_wrappers.py
@@ -1,4 +1,18 @@
+from .majority_voting import *
+from .simple import *
+
 import SimpleITK as sitk
+
+direct_itk_methods = ['staple', 'itkvoting']
+
+def fuse_segmentations_nonITK(list_of_oneHotEncodedSegmentations, method, class_list):
+  '''
+  This function takes a list of one-hot encoded segmentations and the method as input and returns the one-hot encoded fused segmentation for non-ITK implementations
+  '''
+  if 'majority' in method:
+    return majority_voting(list_of_oneHotEncodedSegmentations)
+  elif 'simple' in method:
+    return simple_iterative(list_of_oneHotEncodedSegmentations)
 
 def fuse_segmentations_itk(list_of_segmentations_images, method):
   '''
