d42ebd81b7ceec0f839095e39d02bb1c8aeb0c2f
sarthakpati <sarthak.pati@hotmail.com>
2021-03-23 08:02:31 -0400

    trying something

--- LabelFusion/fusion_wrappers.py
+++ LabelFusion/fusion_wrappers.py
@@ -19,7 +19,14 @@ def fuse_segmentations_itk(list_of_segmentations_images, method):
   This function takes a list of segmentations images and the method as input and returns the fused segmentation for ITK implementations
   '''
   if 'staple' in method:
-    return sitk.MultiLabelSTAPLE(list_of_segmentations_images) # DOI: 10.1109/TMI.2004.830803
+    filter = sitk.MultiLabelSTAPLEImageFilter()
+    filter.SetLabelForUndecidedPixels(0)
+    filter.SetMaximumNumberOfIterations(50)
+    filter.SetDebug(True)
+    test = filter.Execute(list_of_segmentations_images)
+    # test = filter.ComputeMaximumInputValue()
+    
+    return test # sitk.MultiLabelSTAPLE(list_of_segmentations_images, 3) # DOI: 10.1109/TMI.2004.830803
   elif 'voting' in method:
     votingFilter = sitk.LabelVotingImageFilter()
     votingFilter.SetLabelForUndecidedPixels(0)
